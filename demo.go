df9c453c4d92391379b0f0bc4ca3b24b0ad3d136
Florin Patan <florinpatan@gmail.com>
2018-05-25 10:49:39 +0300

    Initial commit

---
+++ demo.go
@@ -0,0 +1,11 @@
+package demo
+
+type MyDemoType struct {}
+
+func (m MyDemoType) name() {
+
+}
+
+func (m MyDemoType) name3() {
+
+}
---
+++ demo2.go
@@ -0,0 +1,9 @@
+package demo
+
+func (m MyDemoType) name2() {
+
+}
+
+func (m MyDemoType) name4() {
+
+}
